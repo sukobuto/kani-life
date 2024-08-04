9455335c02008af540d2d5650bff405f9f989aeb
Kenta Suzuki <k-suzuki@mi-labo.co.jp>
2024-08-04 14:50:39 +0900

    backend serves static files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tower_http::services::ServeDir;
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    // ServeDir falls back to serve index.html when requesting a directory
+    let router = Router::new().nest_service("/", ServeDir::new("static"));
+
+    Ok(router.into())
 }
